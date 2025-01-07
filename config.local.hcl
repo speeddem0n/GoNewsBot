f464ca394d3593048111a14e7ed3499e6ba5e2c0
speeddem0n <slavik2890@gmail.com>
2025-01-08 02:27:28 +0300

    changed gitignore v2

--- config.local.hcl
+++
@@ -1,3 +0,0 @@
-telegram_bot_token = "7759181131:AAGTTPYsxai6cxFAW7YzBwgThQC6PZj85xM"
-telegram_channel_id = 123
-
--- internal/config/config.go
+++ internal/config/config.go
@@ -29,8 +29,8 @@ var ( // Переменные cfg  для записи конфига и once sy
 func Get() Config { // Функция для получения конфига
 	once.Do(func() { // Получаем конфиг с помошью once.Do
 		loader := aconfig.LoaderFor(&cfg, aconfig.Config{ // LoaderFor создает новый Loader на основе структуры Config
-			EnvPrefix: "NFB",                                                                                    // Задаем префиксс для переменных окружения
-			Files:     []string{"./config.hcl", "./config.local.hcl", "$HOME/.config/news-feed-bot/config.hcl"}, // Файлы где хранится конфиг
+			EnvPrefix: "NFB",                                                                                                                    // Задаем префиксс для переменных окружения
+			Files:     []string{"./internal/config/config.hcl", "./internal/config/config.local.hcl", "$HOME/.config/news-feed-bot/config.hcl"}, // Файлы где хранится конфиг
 			FileDecoders: map[string]aconfig.FileDecoder{ // Декодер для файлов hcl
 				".hcl": aconfighcl.New(),
 			},
